898a4764bf18f113918c94def1e204e83bde905c
ToxicKidz <78174417+ToxicKidz@users.noreply.github.com>
2021-02-26 16:38:12 -0500

    chore: delete source code command and add docstrings

--- cogs/utility/info.py
+++ cogs/utility/info.py
@@ -10,18 +10,25 @@ from pathlib import Path
 
 GITHUB_REPO_URL = "https://github.com/vcokltfre/Magoji"
 
+
 class SourceConverter(commands.Converter):
     """A Converter that converts a string to a Command, Cog or Extension."""
-    async def convert(self, ctx: commands.Context, argument: str) -> Union[commands.Command, commands.Cog, ModuleType]:
+
+    async def convert(
+        self, ctx: commands.Context, argument: str
+    ) -> Union[commands.Command, commands.Cog, ModuleType]:
         if command := ctx.bot.get_command(argument):
+            if command.name == "help":
+                return ctx.bot.help_command
             return command
-        
-        if cog := ctx.bot.get_command(argument):
+
+        if cog := ctx.bot.get_cog(argument):
             return cog
 
         if extension := ctx.bot.extensions.get(argument):
             return extension
-        raise commands.BadArgument('Not a valid Command, Cog nor Extension.')
+        raise commands.BadArgument("Not a valid Command, Cog nor Extension.")
+
 
 class AllInfo(commands.Cog):
     def __init__(self, bot):
@@ -30,10 +37,9 @@ class AllInfo(commands.Cog):
     def convert_date(self, date: datetime):
         """Converts a datetime object to a formatted string."""
 
-        return date.strftime(
-            f"%A, %B %-d, %Y at %-I:%M {('A', 'P')[date == 0]}M UTC")
+        return date.strftime(f"%A, %B %-d, %Y at %-I:%M {('A', 'P')[date == 0]}M UTC")
 
-    @commands.command(name='guild', aliases=['server'])
+    @commands.command(name="guild", aliases=["server"])
     async def _guild(self, ctx):
         """Sends information on the guild the command was invoked in."""
         guild = ctx.guild
@@ -44,50 +50,62 @@ class AllInfo(commands.Cog):
         emojis = len(guild.emojis)
         members = guild.members
 
-        presences = {'online': 0,
-                     'offline': 0,
-                     'idle': 0,
-                     'dnd': 0}
+        presences = {"online": 0, "offline": 0, "idle": 0, "dnd": 0}
 
-        member_type = {'user': 0,
-                       'bot': 0}
+        member_type = {"user": 0, "bot": 0}
 
         for member in guild.members:
             presences[str(member.status)] += 1
-            member_type[('user', 'bot')[member.bot]] += 1
+            member_type[("user", "bot")[member.bot]] += 1
 
         online, offline, idle, dnd = presences.values()
         users, bots = member_type.values()
 
-        embed = discord.Embed(title=f"{guild.name.title()}'s Info",
-                              timestamp=datetime.utcnow(),
-                              colour=0x87ceeb)
+        embed = discord.Embed(
+            title=f"{guild.name.title()}'s Info",
+            timestamp=datetime.utcnow(),
+            colour=0x87CEEB,
+        )
 
-        embed.description = textwrap.dedent(f"""
+        embed.description = textwrap.dedent(
+            f"""
             **Owner:** {owner.mention}
             **Created At:** {created}
             **Emojis:** {emojis}
             **Roles:** {num_roles}
-        """)
+        """
+        )
 
-        embed.add_field(name=f"**Total Member Count:** {len(members)}", value=textwrap.dedent(f"""
+        embed.add_field(
+            name=f"**Total Member Count:** {len(members)}",
+            value=textwrap.dedent(
+                f"""
             Users: {users}
             Bots: {bots}
             Online: {online}
             Offline: {offline}
             Idle: {idle}
             Dnd: {dnd}
-        """))
+        """
+            ),
+        )
 
-        embed.add_field(name=f"**Total Channel Count:** {len(guild.channels)}",
-                        value='\n'.join(f"{category.name}: {len(category.channels)}" for category in guild.categories))
+        embed.add_field(
+            name=f"**Total Channel Count:** {len(guild.channels)}",
+            value="\n".join(
+                f"{category.name}: {len(category.channels)}"
+                for category in guild.categories
+            ),
+        )
 
         embed.set_thumbnail(url=guild.icon_url)
-        embed.set_footer(text=f"Command Invoked by {ctx.author}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(
+            text=f"Command Invoked by {ctx.author}", icon_url=ctx.author.avatar_url
+        )
 
         await ctx.send(embed=embed)
 
-    @commands.command(name='user', aliases=['member'])
+    @commands.command(name="user", aliases=["member"])
     async def _user(self, ctx, user: Union[discord.Member, discord.User] = None):
         """Sends info related to the user."""
 
@@ -100,72 +118,74 @@ class AllInfo(commands.Cog):
 
         roles = user.roles[1:]
 
-        embed = discord.Embed(title=f"{str(user).title()}'s Info",
-                              colour=0x87ceeb)
+        embed = discord.Embed(title=f"{str(user).title()}'s Info", colour=0x87CEEB)
 
-        embed.description = textwrap.dedent(f"""
+        embed.description = textwrap.dedent(
+            f"""
                     **User:** {user.mention}
                     **ID:** {user.id}
                     **Created At:** {created}
-                """)
+                """
+        )
 
-        embed.add_field(name="**Member Info**", value=textwrap.dedent(f"""
+        embed.add_field(
+            name="**Member Info**",
+            value=textwrap.dedent(
+                f"""
                     Joined At: {joined}
                     Roles: {', '.join(r.mention for r in reversed(roles))}
-                """))
+                """
+            ),
+        )
 
         embed.set_thumbnail(url=user.avatar_url)
 
         await ctx.send(embed=embed)
 
-    @commands.group(aliases=('src',), invoke_without_command=True)
-    async def source(self, ctx: commands.Context, *, source_item: SourceConverter=None):
-        await self.github(ctx, source_item=source_item)
-    
-    @source.command()
-    async def github(self, ctx: commands.Context, *, source_item: SourceConverter=None):
+    @commands.group(aliases=("src", "github", "git"), invoke_without_command=True)
+    async def source(
+        self, ctx: commands.Context, *, source_item: SourceConverter = None
+    ):
+        """Shows the github repo for this bot, include a command, cog, or extension to got to that file."""
         if source_item is None:
-            embed = discord.Embed(title="Magoji's Github Repository", description=f"[Here's the github link!]({GITHUB_REPO_URL})")
+            embed = discord.Embed(
+                title="Magoji's Github Repository",
+                description=f"[Here's the github link!]({GITHUB_REPO_URL})",
+                colour=0x87CEEB,
+            )
             return await ctx.send(embed=embed)
         embed = self.build_embed(source_item)
         await ctx.send(embed=embed)
-        
-    def get_github_url(self, source_item):    
-        if isinstance(source_item, commands.Command):
+
+    def get_github_url(self, source_item):
+        if isinstance(source_item, (commands.HelpCommand, commands.Cog)):
+            src = type(source_item)
+            filename = getsourcefile(src)
+        elif isinstance(source_item, commands.Command):
             src = source_item.callback.__code__
             filename = src.co_filename
         elif isinstance(source_item, ModuleType):
             src = source_item
             filename = src.__file__
-        else:
-            src = type(source_item)
-            filename = getsourcefile(src)
 
         lines, first_line_no = self.get_source_code(source_item)
         if first_line_no:
             lines_extension = f"#L{first_line_no}-L{first_line_no+len(lines)-1}"
         lines_extension = lines_extension or ""
-            
+
         file_location = Path(filename).relative_to(Path.cwd()).as_posix()
 
         url = f"{GITHUB_REPO_URL}/blob/master/{file_location}{lines_extension}"
 
         return url, file_location, first_line_no or None
 
-    @source.command()
-    async def code(self, ctx: commands.Context, *, source_item: SourceConverter):
-        source = self.get_source_code(source_item)[0]
-        source = '\n'.join(source)
-        source_name = getattr(source_item, 'name', False) or getattr(source_item, '__name__')
-        escaped = source.replace('```', '`\u200b' * 3)
-        embed = discord.Embed(title=f'Source Code for `{source_name}`',
-                              description=f"```py\n{escaped}```",
-                              colour=0x87CEEB)
-        await ctx.send(embed=embed)
-
-    def get_source_code(self, source_item: Union[commands.Command, commands.Cog, ModuleType]) -> Tuple[str, int]:
+    def get_source_code(
+        self, source_item: Union[commands.Command, commands.Cog, ModuleType]
+    ) -> Tuple[str, int]:
         if isinstance(source_item, (commands.Cog, ModuleType)):
             source = getsourcelines(source_item)
+        elif isinstance(source_item, commands.HelpCommand):
+            source = getsourcelines(type(source_item))
         elif isinstance(source_item, commands.Command):
             source = getsourcelines(source_item.callback)
 
@@ -177,7 +197,8 @@ class AllInfo(commands.Cog):
 
         if isinstance(source_object, commands.HelpCommand):
             title = "Help Command"
-            description = source_object.__doc__.splitlines()[1]
+            help_cmd = self.bot.get_command("help")
+            description = help_cmd.help
         elif isinstance(source_object, commands.Command):
             description = source_object.short_doc
             title = f"Command: {source_object.qualified_name}"
@@ -187,13 +208,13 @@ class AllInfo(commands.Cog):
             title = f"Cog: {source_object.qualified_name}"
             description = source_object.description.splitlines()[0]
 
-        embed = discord.Embed(title=title, description=description, colour = 0x87CEEB)
+        embed = discord.Embed(title=title, description=description, colour=0x87CEEB)
         embed.add_field(name="Source Code", value=f"[Here's the Github link!]({url})")
         line_text = f":{first_line}" if first_line else ""
         embed.set_footer(text=f"{location}{line_text}")
 
         return embed
-        
+
 
 def setup(bot):
     bot.add_cog(AllInfo(bot))
