fe4397660b4b81cc51b8bc1760665d286df79f89
ChiliPy <kaspian.nunno@icloud.com>
2021-02-26 13:57:13 -0600

    fix: changed to using the embed helper and fixed the helper

--- cogs/utility/info.py
+++ cogs/utility/info.py
@@ -5,24 +5,20 @@ from datetime import datetime
 import textwrap
 from typing import Union
 
+from utilities.helpers import EmbedHelper, convert_date
+
 
 class AllInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def convert_date(self, date: datetime):
-
-        # TODO: Add docstring
-
-        return date.strftime(
-            f"%A, %B %-d, %Y at %-I:%M {('A', 'P')[date == 0]}M UTC")
-
     @commands.command(name='guild', aliases=['server'])
-    async def _guild(self, ctx):
+    @commands.guild_only()
+    async def _guild(self, ctx: commands.Context):
         """Sends information on the guild the command was invoked in."""
         guild = ctx.guild
         owner = guild.owner
-        created = self.convert_date(guild.created_at)
+        created = convert_date(guild.created_at)
 
         num_roles = len(guild.roles)
         emojis = len(guild.emojis)
@@ -34,71 +30,90 @@ class AllInfo(commands.Cog):
                      'dnd': 0}
 
         member_type = {'user': 0,
+                       'staff': 0,
                        'bot': 0}
 
-        for member in guild.members:
+        for member in members:
             presences[str(member.status)] += 1
             member_type[('user', 'bot')[member.bot]] += 1
+            if (member.guild_permissions.ban_members or
+                    member.guild_permissions.kick_members or
+                    member.guild_permissions.manage_messages):
+                member_type['staff'] += 1
 
         online, offline, idle, dnd = presences.values()
-        users, bots = member_type.values()
+        users, staff, bots = member_type.values()
 
-        embed = discord.Embed(title=f"{guild.name.title()}'s Info",
-                              timestamp=datetime.utcnow(),
-                              colour=0x87ceeb)
-
-        embed.description = textwrap.dedent(f"""
-            **Owner:** {owner.mention}
-            **Created At:** {created}
-            **Emojis:** {emojis}
-            **Roles:** {num_roles}
-        """)
-
-        embed.add_field(name=f"**Total Member Count:** {len(members)}", value=textwrap.dedent(f"""
-            Users: {users}
-            Bots: {bots}
+        value2 = textwrap.dedent(f"""
             Online: {online}
             Offline: {offline}
             Idle: {idle}
-            Dnd: {dnd}
-        """))
+            DnD: {dnd}
+            """)
 
-        embed.add_field(name=f"**Total Channel Count:** {len(guild.channels)}",
-                        value='\n'.join(f"{category.name}: {len(category.channels)}" for category in guild.categories))
+        value1 = textwrap.dedent(f"""
+            Members: {len(members)}
+            Users: {users}
+            Staff: {staff}
+            Bots: {bots}
+            """)
 
-        embed.set_thumbnail(url=guild.icon_url)
-        embed.set_footer(text=f"Command Invoked by {ctx.author}", icon_url=ctx.author.avatar_url)
+        value3 = textwrap.dedent(f"""
+                                Category Channels: {len(guild.categories)}
+                                Voice Channels: {len(guild.voice_channels)}
+                                Text Channels: {len(guild.text_channels)}
+                                """)
+
+        embed = EmbedHelper(title=f"{guild.name.title()}'s Info",
+                            timestamp=datetime.utcnow(),
+                            description=textwrap.dedent(f"""
+                            **Owner:** {owner.mention}
+                            **Created At:** {created}
+                            **Emojis:** {emojis}
+                            **Roles:** {num_roles}
+                                    """),
+
+                            thumbnail_url=guild.icon_url,
+
+                            footer_text=f"Command Invoked by {ctx.author}",
+                            footer_url=ctx.author.avatar_url,
+
+                            fields=[
+                                {"name": f"**Member Count:**", "value": value1},
+                                {"name": "**Presences**", "value": value2},
+                                {"name": f"**Total Channel Count:** {len(guild.channels)}", "value": value3, "inline": False}
+                            ]
+                            )
 
         await ctx.send(embed=embed)
 
     @commands.command(name='user', aliases=['member'])
-    async def _user(self, ctx, user: Union[discord.Member, discord.User] = None):
+    async def _user(self, ctx: commands.Context, user: Union[discord.Member, discord.User] = None):
         """Sends info related to the user."""
 
         # TODO: Add comments
 
         user = user or ctx.author
 
-        created = self.convert_date(user.created_at)
-        joined = self.convert_date(user.joined_at)
+        created = convert_date(user.created_at)
+        joined = convert_date(user.joined_at)
 
         roles = user.roles[1:]
 
-        embed = discord.Embed(title=f"{str(user).title()}'s Info",
-                              colour=0x87ceeb)
-
-        embed.description = textwrap.dedent(f"""
-                    **User:** {user.mention}
-                    **ID:** {user.id}
-                    **Created At:** {created}
-                """)
-
-        embed.add_field(name="**Member Info**", value=textwrap.dedent(f"""
+        value = textwrap.dedent(f"""
                     Joined At: {joined}
                     Roles: {', '.join(r.mention for r in reversed(roles))}
-                """))
+                """)
 
-        embed.set_thumbnail(url=user.avatar_url)
+        embed = EmbedHelper(title=f"{str(user).title()}'s Info",
+                            description=textwrap.dedent(f"""
+                            **User:** {user.mention}
+                            **ID:** {user.id}
+                            **Created At:** {created}
+                        """),
+
+                            thumbnail_url=user.avatar_url,
+                            fields=[{"name": "**Member Info**", "value": value}])
 
         await ctx.send(embed=embed)
 
