ba94b8faff5226594ee27ffbcd2738266d3d6fd5
ChiliPy <kaspian.nunno@icloud.com>
2021-03-02 13:08:15 -0600

    feat: added tempban command

--- cogs/moderation/moderation.py
+++ cogs/moderation/moderation.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import textwrap
+from typing import Optional
 
 from utilities.helpers import EmbedHelper, CustomTimeConverter
 from internal.context import Context
@@ -66,12 +67,13 @@ class StaffCommands(commands.Cog):
 
             await ctx.send(embed=embed)
 
-    @commands.command(name="ban")
+    @commands.command(name="ban", aliases=['pban'])
     @commands.guild_only()
     @commands.has_permissions(ban_members=True)
-    async def _ban(self, ctx: Context, member: discord.Member, length : CustomTimeConverter, *, reason="Breaking The Rules"):
+    async def _ban(self, ctx: Context, member: discord.Member, *, reason="Breaking The Rules"):
 
         # TODO: add comments and docstrings
+
 
         if member.top_role < ctx.author.top_role:
 
@@ -79,6 +81,8 @@ class StaffCommands(commands.Cog):
                                         **ID:** {member.id}
                                         **Bot:** {member.bot}
                                         **Reason:** {reason.title()}
+                                        **Length:** Permanent
+                                        **Expires:** Never
                                             """)
 
             curtime = datetime.now()
@@ -98,8 +102,8 @@ class StaffCommands(commands.Cog):
             await ctx.send(embed=embed)
 
             await self.bot.db.execute(
-                '''INSERT INTO Cases(id, guildid, userid, modid, username, modname, case_type, created_at)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)''', next(self.bot.idgen), ctx.guild.id, member.id, ctx.author.id, member, ctx.author, "ban", curtime)
+                '''INSERT INTO Cases(id, guildid, userid, modid, username, modname, case_type, created_at, expires_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)''', next(self.bot.idgen), ctx.guild.id, member.id, ctx.author.id, member, ctx.author, "ban", curtime, "never")
 
             await member.ban(reason=reason)
 
@@ -119,6 +123,66 @@ class StaffCommands(commands.Cog):
                                 )
 
             await ctx.send(embed=embed)
+
+
+    @commands.command(aliases=['tban'])
+    @commands.guild_only()
+    @commands.has_permissions(ban_members=True)
+    async def tempban(self, ctx: Context, member: discord.Member, length : CustomTimeConverter, *, reason="Breaking The Rules"):
+
+        # TODO: add comments and docstrings
+
+
+        if member.top_role < ctx.author.top_role:
+
+            curtime = datetime.now()
+            expires = curtime + timedelta(seconds=length)
+
+            description = textwrap.dedent(f"""
+                                        **ID:** {member.id}
+                                        **Bot:** {member.bot}
+                                        **Reason:** {reason.title()}
+                                        **Length:** {length}
+                                        **Expires:** {expires}
+                                            """)
+
+
+            embed = EmbedHelper(
+                title=f"Banning {member}",
+                description=description,
+
+                footer_text=f"Banned by {ctx.author}",
+                footer_url=ctx.author.avatar_url,
+
+                timestamp=curtime,
+                thumbnail_url=member.avatar_url,
+            )
+
+            await ctx.send(embed=embed)
+
+            await self.bot.db.execute(
+                '''INSERT INTO Cases(id, guildid, userid, modid, username, modname, case_type, created_at, expires_at)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)''', next(self.bot.idgen), ctx.guild.id, member.id, ctx.author.id, member, ctx.author, "tempban", curtime, expires)
+
+            await member.ban(reason=reason)
+
+        else:
+
+            embed = EmbedHelper(title=f'An Error Has Occurred',
+                                description=f'''An error has occurred when attempting to ban {member.mention}: 
+                                "cannot ban members with the same/higher role as you"'''.strip('\n'),
+
+                                thumbnail_url=member.avatar_url,
+
+                                timestamp=datetime.now(),
+                                colour=discord.Colour.red(),
+
+                                footer_text=f"Command Attempted by {ctx.author}",
+                                footer_url=ctx.author.avatar_url,
+                                )
+
+            await ctx.send(embed=embed)
+
 
 
 def setup(bot):
