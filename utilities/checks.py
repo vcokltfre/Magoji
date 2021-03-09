5fbf559b39b5dd8e0eeedfe9c9d0e2daa91d0660
ToxicKidz <78174417+ToxicKidz@users.noreply.github.com>
2021-03-09 13:28:53 -0500

    feat: add role_hierarchy check and run_in_executor deco

--- utilities/checks.py
+++ utilities/checks.py
@@ -1,22 +1,43 @@
-from discord.ext.commands import check, Context
-from json import loads
+from discord.ext.commands import check, MissingPermissions
+from functools import wraps
+from discord import Member
 
+from internal.context import Context
 
 def command_enabled(command: str):
     async def predicate(ctx: Context):
         if not ctx.guild:
             return True
 
-        config = await ctx.bot.db.fetch_guild(ctx.guild.id)
+        config = await ctx.guild_config()
 
         if not config:
             return True
 
-        config = loads(config[2]).get("disabled_commands", {})
+        disabled_commands = config.get("disabled_commands", {})
 
-        if not config:
+        if not disabled_commands:
             return True
 
-        return command not in config
+        return command not in disabled_commands
 
     return check(predicate)
+
+def role_hierarchy(ctx_arg: int = 0, member_arg: int = 1):
+    """Check if the invoker's top role is higher than the member's top role."""
+    def decorator(func):
+        @wraps(func)
+        async def inner(*args, **kwargs):
+            ctx = args[ctx_arg]
+            member = args[member_arg] or kwargs.get('member')
+            if not (isinstance(ctx, Context) and isinstance(member, Member)):
+                return await func(*args, **kwargs) # Skip if they aren't the right types.
+            
+            if ctx.author.top_role <= member.top_role:
+                raise MissingPermissions(
+                f"{ctx.author.display_name}'s top role is not higherlower than {member.display_name}'s role."
+                )
+
+            return await func(*args, **kwargs)
+        return inner
+    return decorator
